2c4450271fec2f6058b7c2babc2367541fe7baf7
Remy Raes <contact@remyraes.com>
2023-11-06 19:09:47 +0100

    build: initial commit

---
+++ lib/ble_bootstrap_channel.dart
@@ -0,0 +1,37 @@
+library ble_bootstrap_channel;
+
+import 'package:venice_core/channels/abstractions/bootstrap_channel.dart';
+import 'package:venice_core/channels/channel_metadata.dart';
+import 'package:venice_core/file/file_metadata.dart';
+
+class BleBootstrapChannel extends BootstrapChannel {
+  @override
+  Future<void> close() {
+    // TODO: implement close
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> initReceiver() {
+    // TODO: implement initReceiver
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> initSender() {
+    // TODO: implement initSender
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendChannelMetadata(ChannelMetadata data) {
+    // TODO: implement sendChannelMetadata
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> sendFileMetadata(FileMetadata data) {
+    // TODO: implement sendFileMetadata
+    throw UnimplementedError();
+  }
+}
