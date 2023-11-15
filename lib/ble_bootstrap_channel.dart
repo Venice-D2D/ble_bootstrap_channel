1f0187364be2d40954890b22606b0a64d63206be
Remy Raes <contact@remyraes.com>
2023-11-15 16:11:53 +0100

    feat: sender advertises itself as "venice"

--- lib/ble_bootstrap_channel.dart
+++ lib/ble_bootstrap_channel.dart
@@ -3,7 +3,9 @@
 library ble_bootstrap_channel;
 
 import 'dart:async';
+import 'dart:typed_data';
 
+import 'package:bluetooth_low_energy/bluetooth_low_energy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:venice_core/channels/abstractions/bootstrap_channel.dart';
@@ -12,6 +14,7 @@ import 'package:venice_core/file/file_metadata.dart';
 
 class BleBootstrapChannel extends BootstrapChannel {
   final BuildContext context;
+  PeripheralManager get peripheralManager => PeripheralManager.instance;
   BleBootstrapChannel(this.context);
 
   @override
@@ -95,15 +98,52 @@ class BleBootstrapChannel extends BootstrapChannel {
   }
 
   @override
-  Future<void> initSender() {
-    // TODO: implement initSender
-    throw UnimplementedError();
+  Future<void> initSender() async {
+    await peripheralManager.clearServices();
+    final service = GattService(
+      uuid: UUID.short(100),
+      characteristics: [
+        GattCharacteristic(
+          uuid: UUID.short(200),
+          properties: [
+            GattCharacteristicProperty.read,
+          ],
+          descriptors: [],
+        ),
+        GattCharacteristic(
+          uuid: UUID.short(201),
+          properties: [
+            GattCharacteristicProperty.read,
+            GattCharacteristicProperty.write,
+            GattCharacteristicProperty.writeWithoutResponse,
+          ],
+          descriptors: [],
+        ),
+        GattCharacteristic(
+          uuid: UUID.short(202),
+          properties: [
+            GattCharacteristicProperty.notify,
+            GattCharacteristicProperty.indicate,
+          ],
+          descriptors: [],
+        ),
+      ],
+    );
+    await peripheralManager.addService(service);
+    final advertisement = Advertisement(
+      name: 'venice',
+      manufacturerSpecificData: ManufacturerSpecificData(
+        id: 0x2e19,
+        data: Uint8List.fromList([0x01, 0x02, 0x03]),
+      ),
+    );
+    await peripheralManager.startAdvertising(advertisement);
   }
 
   @override
-  Future<void> sendChannelMetadata(ChannelMetadata data) {
-    // TODO: implement sendChannelMetadata
-    throw UnimplementedError();
+  Future<void> sendChannelMetadata(ChannelMetadata data) async {
+    await peripheralManager.setUp();
+    initSender();
   }
 
   @override
