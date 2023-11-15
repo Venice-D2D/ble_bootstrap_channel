5c9ee826392a143f1ca95e48e15d439b359d7515
Remy Raes <contact@remyraes.com>
2023-11-15 16:06:49 +0100

    build: add bluetooth_low_energy dependency

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import bluetooth_low_energy_darwin
 import flutter_blue_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BluetoothLowEnergyDarwin.register(with: registry.registrar(forPlugin: "BluetoothLowEnergyDarwin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
 }
